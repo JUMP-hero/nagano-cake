c38d858cf49397460c676ab99ce7d16523247feb
takkamu <104718319+takkamu@users.noreply.github.com>
2022-06-17 12:11:09 +0900

    Revert "Develop"

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.hosts << "a5c50e11c8924089a0cb810d70fdc5bc.vfs.cloud9.us-east-1.amazonaws.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
